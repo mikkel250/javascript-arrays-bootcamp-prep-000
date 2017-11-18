8b820a34f788a1b6d0fe6aa9729a514513fefd69
Mikkel Ridley <mikkel250@gmail.com>
2017-11-18 06:09:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,3 +5,7 @@ function addElementToBeginningOfArray(array, element) {
   return newArray;
 }
 
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element, ...array];
+}
+
