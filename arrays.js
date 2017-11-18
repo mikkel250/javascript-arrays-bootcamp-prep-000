797abd4ce6ce6472f44d2e1d754cc070c8949091
Mikkel Ridley <mikkel250@gmail.com>
2017-11-18 06:21:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,6 +24,9 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 
-
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift;
+  return array;
+}
 
 
