0201c0d97fea89eab00f355594049ed3c452bc2c
Mikkel Ridley <mikkel250@gmail.com>
2017-11-18 06:32:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,8 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1);
-  return array;
+  return array.slice(1);
+  
 }
 
 
